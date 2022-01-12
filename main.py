456758fd9511ea9768a9dce3fc6a78adc4dbd5b2
Sayidbek Bahromov <87595572+sayidbek-bakhrom@users.noreply.github.com>
2022-01-12 19:10:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+top_of_range = input("Type a number: ")
+print(f"Well, I am thinking a number between 1 and {top_of_range}.")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+while True:
+    user_guess = input("Make a guess: ")
+    guesses += 1
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it")
+        break
+    elif user_guess > random_number:
+        print("Too high")
+    else:
+        print("Too less.")
+
+print(f"You got it in {guesses} guesses.")
